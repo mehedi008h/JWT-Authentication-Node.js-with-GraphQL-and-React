8d62ceebc48b5eb98064ba7972a6d1b0e831a1d6
Mehedi Hasan <mehedi08h@gmail.com>
2022-08-05 21:01:35 +0600

    implement cookie

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,22 @@
-import { Resolver, Query, Arg, Mutation } from "type-graphql";
+import {
+    Resolver,
+    Query,
+    Arg,
+    Mutation,
+    ObjectType,
+    Field,
+    Ctx,
+} from "type-graphql";
 import { User } from "./entity/User";
 import { hash, compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { MyContext } from "./MyContext";
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -12,6 +28,43 @@ export class UserResolver {
     @Query(() => [User])
     users() {
         return User.find();
+    }
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg("email") email: string,
+        @Arg("password") password: string,
+        @Ctx() { res }: MyContext
+    ): Promise<LoginResponse> {
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            throw new Error("Could not find user");
+        }
+
+        const valid = await compare(password, user.password);
+
+        if (!valid) {
+            throw new Error("Bad password");
+        }
+
+        // login successful
+
+        res.cookie(
+            "auth",
+            sign({ userId: user.id }, "hdsdhfjdnfdj", {
+                expiresIn: "7d",
+            }),
+            {
+                httpOnly: true,
+            }
+        );
+
+        return {
+            accessToken: sign({ userId: user.id }, "sdfhsdf", {
+                expiresIn: "15m",
+            }),
+        };
     }
 
     @Mutation(() => Boolean)
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,15 @@ import { AppDataSource } from "./data-source";
 import { User } from "./entity/User";
 import "reflect-metadata";
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const { ApolloServer } = require("apollo-server-express");
 const { buildSchema } = require("type-graphql");
 const { UserResolver } = require("./UserResolver");
 
 (async () => {
     const app = express();
+    app.use(cookieParser());
+
     app.get("/", (req, res) => res.send("Hello"));
 
     AppDataSource.initialize();
@@ -19,6 +22,7 @@ const { UserResolver } = require("./UserResolver");
     const apploServer = new ApolloServer({
         schema,
         playground: true,
+        context: ({ req, res }) => ({ req, res }),
     });
     await apploServer.start();
     apploServer.applyMiddleware({ app });
